429e1d7cb8675da09e778e38b5d05d537f541eca
raman <olagunjuraman@gmail.com>
2021-01-08 12:01:06 +0100

    identifier-added

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const axios = require("axios");
-const uuid = require("uuid");
+
+const randomString = (length) => {
+  charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randomString = "";
+  for (let i = 0; i < length; i++) {
+    const randomPoz = Math.floor(Math.random() * charSet.length);
+    randomString += charSet.substring(randomPoz, randomPoz + 1);
+  }
+  return randomString;
+};
 
 const app = express();
 
@@ -8,17 +17,15 @@ app.use(express.json());
 
 const router = express.Router();
 
-const convertToCsv = async (req, res) => {
+const extractStudentsinfo = async (req, res) => {
   const {
     csv: { url, select_fields },
   } = req.body;
 
-  // const select_fields = req.body.csv.select_fields;
-
   const { data } = await axios.get(url);
 
-  const convertCSVToJSON = (str, delimiter = ",", fields) => {
-    const titles = str.slice(0, str.indexOf("\r\n")).split(",");
+  const convertCSVToJSON = (str, delimiter = ",") => {
+    const titles = str.slice(0, str.indexOf("\r\n")).split(delimiter);
     const rows = str.slice(str.indexOf("\r\n") + 2).split("\r\n");
     return rows.map((row) => {
       // Convert to 2D array
@@ -38,15 +45,17 @@ const convertToCsv = async (req, res) => {
   };
 
   const result = res.json({
-    conversion_key: uuid.v4(),
+    conversion_key: randomString(32),
     json: convertCSVToJSON(data.trim(), select_fields),
   });
 
   return result;
 };
 
-router.route("/").post(convertToCsv);
+router.route("/").post(extractStudentsinfo);
 
 app.use("/api/v1/csv", router);
 
-app.listen(6000, console.log("Server listening succesfully"));
+const PORT = 7000;
+
+app.listen(PORT, console.log(`Server listening succesfully on port ${PORT}`));
